afc1c165b3d653d0043b5ae5289de03aa108654e
Thomas Fontaine <thomas.fontaine@ubisoft.com>
2019-11-29 16:46:25 +0100

    [PACKAGE] - Adding basic project to test package import

---
+++ DynamicHourLight/Runtime/LightingPreset.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+
+[System.Serializable]
+[CreateAssetMenu(fileName = "Lightning Preset", menuName = "Scriptables/Lightning Preset", order = 1)]
+public class LightingPreset : ScriptableObject
+{
+    public Gradient ambientColor;
+    public Gradient DirectionalColor;
+    public Gradient fogColor;
+}
